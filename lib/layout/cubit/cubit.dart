a569376fbe0abe1184928a730e1bfaa7bfbbd830
ZieadEmad <zieadwjsjs@users.noreply.github.com>
2021-02-20 23:11:12 +0200

    V 2.5

--- lib/layout/cubit/cubit.dart
+++ lib/layout/cubit/cubit.dart
@@ -2,9 +2,9 @@
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:food_project/layout/cubit/states.dart';
-import 'file:///G:/Flutter_Pro/food_project/lib/screens/home/home_screen.dart';
 import 'package:food_project/screens/cart/cart_screen.dart';
 import 'package:food_project/screens/favorites/favorites_screen.dart';
+import 'package:food_project/screens/home/home_screen.dart';
 import 'package:food_project/screens/orders/orders_screen.dart';
 import 'package:food_project/screens/profile/profile_screen.dart';
 
